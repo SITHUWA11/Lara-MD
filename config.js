0303c3e1432c13046a2a0ba9462b5355d53480c7
SITHUWA11 <Wadagaming14@gmail.com>
2024-12-12 21:44:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nmohGYRC#UmO5fMaGQjZFTbjjRkACruyqzL6S46ZYapkq2WwBLIs", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "K7pkHShY#GPFQwE-4MgoxEV5NPQkbP_Q0_C4qzGI1v5DaAdjIL20", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
